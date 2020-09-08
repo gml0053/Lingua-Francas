d6fc792ed9242fe05fede82b43979463e65187f0
Hanna <hanna.flores@unt.edu>
2020-09-08 15:49:16 -0500

    set up mongoose and connect to mongodb atlas

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//this pulls in our environment variables
+require('dotenv').config()
+
+
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}); // connect to our database
 
 app.get('/', function(req, res) {
     res.send('Hello world!');
